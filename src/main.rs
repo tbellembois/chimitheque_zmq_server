44f29ebcc902c0a0254c8f780f452ea1c5d1b8b1
Thomas Bellembois <thomas.bellembois@uca.fr>
2023-11-06 16:19:11 +0100

    First commit.

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use chimitheque_utils::{
+    casnumber::is_cas_number, cenumber::is_ce_number, formula::empirical_formula,
+    requestfilter::request_filter,
+};
+
+use log::{debug, error, info};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+enum Request {
+    IsCasNumber(String),
+    IsCeNumber(String),
+    EmpiricalFormula(String),
+    RequestFilter(String),
+}
+
+fn main() {
+    env_logger::init();
+
+    // Connect to socket.
+    let context = zmq::Context::new();
+    let responder = context.socket(zmq::REP).unwrap();
+    assert!(responder.bind("tcp://*:5556").is_ok());
+
+    // Input message variable.
+    let mut maybe_raw_message = zmq::Message::new();
+
+    info!("Waiting for messages.");
+
+    loop {
+        // Waiting for incoming messages.
+        if let Err(e) = responder.recv(&mut maybe_raw_message, 0) {
+            error!("error receiving message: {e}");
+        } else {
+            debug!("maybe_raw_message: {:?}", maybe_raw_message.as_str());
+
+            // FIXME: change default response.
+            let mut response: Result<Box<dyn erased_serde::Serialize>, String> =
+                Err("empty response".to_string());
+
+            // Decoding message into Request.
+            if let Some(message) = maybe_raw_message.as_str() {
+                match serde_json::from_str::<Request>(message) {
+                    Ok(request) => {
+                        info!("request: {:?}", request);
+
+                        // Do the job.
+                        match request {
+                            Request::IsCasNumber(s) => {
+                                info!("IsCasNumber({s})");
+                                response = match is_cas_number(&s) {
+                                    Ok(o) => Ok(Box::new(o)),
+                                    Err(e) => Err(e),
+                                };
+                            }
+                            Request::IsCeNumber(s) => {
+                                info!("IsCeNumber({s})");
+                                response = match is_ce_number(&s) {
+                                    Ok(o) => Ok(Box::new(o)),
+                                    Err(e) => Err(e),
+                                };
+                            }
+                            Request::EmpiricalFormula(s) => {
+                                info!("EmpiricalFormula({s})");
+                                response = match empirical_formula(&s) {
+                                    Ok(o) => Ok(Box::new(o)),
+                                    Err(e) => Err(e),
+                                };
+                            }
+                            Request::RequestFilter(s) => {
+                                info!("RequestFilter({s})");
+                                response = match request_filter(&s) {
+                                    Ok(o) => Ok(Box::new(o)),
+                                    Err(e) => Err(e),
+                                };
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        error!("error deserializing message: {e}");
+                        response = Err(e.to_string());
+                    }
+                };
+            }
+
+            // Serialize response.
+            match serde_json::to_string(&response) {
+                Ok(serialized_response) => {
+                    info!("response: {:#?}", serialized_response);
+                    if let Err(e) = responder.send(&serialized_response, 0) {
+                        error!("error sending response: {e}");
+                    };
+                }
+                Err(e) => {
+                    error!("error serializing response: {e}");
+                    if let Err(e) = responder.send(&e.to_string(), 0) {
+                        error!("error sending response: {e}");
+                    };
+                }
+            };
+        }
+    }
+}
